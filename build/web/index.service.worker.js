f998bc464517c12ac1d7fd07c1c738856c08093a
Aaron Park <apark@Aerons-Mac-mini.local>
2026-03-26 13:31:53 -0400

    Restore emojis - font is working

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774546186|1140146';
+const CACHE_VERSION = '1774546279|1159608';
 /** @type {string} */
 const CACHE_PREFIX = 'Fruit Love Islan-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
